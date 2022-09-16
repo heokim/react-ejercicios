9e9382ad046a94756c29c9da9006113ea58f789d
heokim <andresssshk@gmail.com>
2022-09-16 14:38:46 -0400

    Ejercicios 1 - crud con datos locales

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CrudApp from "./components/CrudApp";
+
+function App() {
+  return (
+    <>
+      <h1>ejercicios con React</h1>
+      <CrudApp />
+    </>
+  );
+}
+
+export default App;
